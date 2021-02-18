be8569b7b61c6a3b701a588994108170ac8be8b9
devkeita <keita.mywork@gmail.com>
2021-02-18 21:23:23 +0900

    serializerを導入

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,14 +1,19 @@
 class V1::PostsController < ApplicationController
   before_action :set_post, only: %i[show edit destroy]
 
+  def index
+    posts = Post.order(created_at: :desc).limit(20)
+    render json: posts
+  end
+
   def show
-    render json: { post: @post }
+    render json: @post
   end
 
   def create
     post = Post.new(post_params)
     if post.save
-      render json: { post: post }
+      render json: post
     else
       render json: { errors: post.errorr }
     end
@@ -16,7 +21,7 @@ class V1::PostsController < ApplicationController
 
   def edit
     if @post.update(post_params)
-      render json: { post: @post }
+      render json: @post
     else
       render json: { errors: @post.errors }
     end
@@ -24,7 +29,7 @@ class V1::PostsController < ApplicationController
 
   def destroy
     @post.destroy
-    render json: { post: @post }
+    render json: @post
   end
 
   private
