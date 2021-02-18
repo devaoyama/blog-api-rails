653e102311425c9e289ef6cdffc3ba4b6272efdf
devkeita <keita.mywork@gmail.com>
2021-02-18 20:35:06 +0900

    postを作成

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :post do
-    title { "" }
-    body { "MyString" }
+    title { "MyTitle" }
+    body { "MyBody" }
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "title" do
+    context "blankのときに" do
+      it 'invalidになる' do
+        post = build(:post, title: "")
+        expect(post).not_to be_valid
+      end
+    end
+  end
 end
